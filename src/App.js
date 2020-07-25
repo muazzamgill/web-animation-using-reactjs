1a9fac5b78a4e0ab03e1b2742b2b75096af4fa29
muazzamgill <muazzam.gill@gmail.com>
2020-07-26 00:01:22 +0500

    intial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
+/*
+import React, {useRef, useEffect} from 'react';
+*/
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import useWebAnimations, {shakeY} from "@wellyshen/use-web-animations";
 function App() {
+/*
+  const element = useRef(null);
+  useEffect(() => {
+      element.current.focus();
+  }, [])*/
+
+
+const { ref, playState, getAnimation } = useWebAnimations({ ...shakeY});
+/*
+const { ref, playState, getAnimation } = useWebAnimations({
+   keyframes: {
+        transform: ["translate(0,0)"], // Move by 500px
+        transform: ["translate(600px,0)"], // Move by 500px
+        //background: ["red", "blue", "green"], // Go through three colors
+    },
+    timing: {
+        //delay: 500, // Start with a 500ms delay
+        duration: 1000, // Run for 1000ms
+        iterations: Infinity, // Repeat once
+        direction: "alternate", // Run the animation forwards and then backwards
+        easing: "ease-in-out", // Use a fancy timing function
+    },
+    onReady: ({ playState, animate, animation }) => {
+        // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+             },
+
+
+  /!*  onUpdate: ({ playState, animate, animation }) => {
+        // Triggered when the animation enters the running state or changes state
+            },
+    onFinish: ({ playState, animate, animation }) => {
+          // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+            }*!/
+
+    // More useful options...
+
+});
+*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+/*
+    <input ref={element} type='text'></input>
+*/
+      <div  className="container">
+        <div className="target" ref={ref}></div>
+        Current animation state: {playState}
+        <button onClick={() => getAnimation().pause()}>Pause</button>
+        <button onClick={() => getAnimation().play()}>Play</button>
+      </div>
   );
 }
 
 export default App;
+
